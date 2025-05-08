e735bca545764577c7099f85c5967864cf8dc2f7
Qenszu <Opalusaktv@gmail.com>
2025-05-08 15:01:42 +0200

    new

--- python/ASD/Cykl_2/kol&&off_past/22_23/off6/zad6.py
+++ python/ASD/Cykl_2/kol&&off_past/22_23/off6/zad6.py
@@ -1,28 +1,36 @@
+# Jakub Worek 17.05.2023 - 21.05.2023
+#
+# Opis algorytmu:
+# Dla każdego pracownika wykonujemy algorytm rekurencyjny w celu znalezienia
+# optymalnego przypisania. 
+# Dokonujemy przypisania pracownika u do maszyny v jeżeli ta jest wolna,
+# lub pracownika m[v] który na niej pracował przeniesiemy na inną.
+# Na końcu zwracamy ilość przypisań, która jest licznością maksymalnego
+# skojarzenia w grafie dwudzielny (pracownicy - maszyny)
+# 
+# Złożoność:
+# O(V*(V+E))
+
 from zad6testy import runtests
+
+def f(G, u, m, s):
+    for v in G[u]:
+        if s[v] == False:
+            s[v] = True
+            if m[v] == -1 or f(G, m[v], m, s):
+                m[v] = u
+                return True
+    return False
 
 def binworker( M ):
     # tu prosze wpisac wlasna implementacje
-    n = len(M)
-    occupied = [-1] * n
-
-    for i in len(n):
-        flag = False
-        for j in range(len(M[i])):
-            if occupied[j] == -1:
-                occupied[j] = M[i][j]
-                flag = True
-        if flag:
-            pass
-
-
-    return 0
-
-M = [ [ 0, 1, 3], # 0
-      [ 2, 4],    # 1
-      [ 0, 2],    # 2
-      [ 3, 4 ],   # 3
-      [ 3, 2] ]   # 4
-binworker(M)
+    n=len(M)
+    m = [-1] * n
+    r = 0
+    for i in range(n):
+        s = [False] * n
+        if f(M, i, m, s): r += 1
+    return r
 
 # zmien all_tests na True zeby uruchomic wszystkie testy
-#runtests( binworker, all_tests = False )
+runtests( binworker, all_tests = True )
