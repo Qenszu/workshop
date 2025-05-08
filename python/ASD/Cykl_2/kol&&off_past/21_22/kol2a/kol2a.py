d3e920dc9e9ef1a94e4123abf5cebacacdd03441
Qenszu <Opalusaktv@gmail.com>
2025-05-08 17:57:47 +0200

    lol

--- python/ASD/Cykl_2/kol&&off_past/21_22/kol2a/kol2a.py
+++ python/ASD/Cykl_2/kol&&off_past/21_22/kol2a/kol2a.py
@@ -1,11 +1,17 @@
 from kol2atesty import runtests
+from queue import PriorityQueue
 
 def drivers( P, B ):
     # tu prosze wpisac wlasna implementacje
 
-    n = len(P)
-
     return []
 
+p = True
+c = False
+P = [(1,c),(3,c),(4,c),(6,c),(8,c),(9,c),(11,c),(13,c),(16,c),(17,c),
+(2,p),(5,p),(7,p),(10,p),(12,p),(14,p),(15,p),(18,p)]
+B = 20
+drivers(P, B)
+
 # zmien all_tests na True zeby uruchomic wszystkie testy
-runtests( drivers, all_tests = False )
+#runtests( drivers, all_tests = False )
